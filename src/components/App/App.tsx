1cde7db976421817cfdcc401a2adda325863a49b
nacchan-kun <sendono.nc17@gmail.com>
2025-05-27 18:29:00 +0300

    app fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,58 @@
-import css from './VoteOptions.module.css';
-import type { VoteType } from '../../types/votes';
+import { useState } from 'react';
+import css from './App.module.css';
+import Notification from '../Notification/Notification';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
 
-interface VoteOptionsProps {
-  onVote: (type: VoteType) => void;
-  onReset: () => void;
-  canReset: boolean;
-}
+import type { Votes, VoteType } from '../../types/votes.ts';
 
-const VoteOptions = ({ onVote, onReset, canReset }: VoteOptionsProps) => {
+const App = () => {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
+    
   return (
-    <div className={css.container}>
-      <button className={css.button} onClick={() => onVote('good')}>
-        Good
-      </button>
-      <button className={css.button} onClick={() => onVote('neutral')}>
-        Neutral
-      </button>
-      <button className={css.button} onClick={() => onVote('bad')}>
-        Bad
-      </button>
-      {canReset && (
-        <button
-          className={`${css.button} ${css.reset}`}
-          onClick={onReset}
-        >
-          Reset
-        </button>
+    <div className={css.app}>
+      <CafeInfo />
+
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+
+    {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
       )}
     </div>
   );
 };
 
-export default VoteOptions;
+export default App;
